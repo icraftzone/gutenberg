e061ab887aca9fa6e91ea6793ec827804a6e2735
Dave Smith <getdavemail@gmail.com>
2020-05-29 12:07:28 +0100

    Update labels and add Create from Pages to dropdown

--- packages/block-library/src/navigation/edit.js
+++ packages/block-library/src/navigation/edit.js
@@ -70,6 +70,7 @@ function Navigation( {
 	className,
 } ) {
 	const CREATE_EMPTY_OPTION_VALUE = '__CREATE_EMPTY__';
+	const CREATE_FROM_PAGES_OPTION_VALUE = '__CREATE_FROM_PAGES__';
 
 	//
 	// HOOKS
@@ -218,7 +219,7 @@ function Navigation( {
 			? [
 					{
 						key: '',
-						name: __( 'Select Menu…' ),
+						name: __( 'Select where to start from…' ),
 					},
 					...menus,
 					{
@@ -228,18 +229,38 @@ function Navigation( {
 					},
 					{
 						key: CREATE_EMPTY_OPTION_VALUE,
-						name: __( 'Create Empty' ),
+						name: __( 'Create empty menu' ),
 					},
+					...( hasPages
+						? [
+								{
+									key: CREATE_FROM_PAGES_OPTION_VALUE,
+									name: __(
+										'Create from all top-level pages'
+									),
+								},
+						  ]
+						: [] ),
 			  ]
 			: [
 					{
 						key: '',
-						name: __( 'Select Menu…' ),
+						name: __( 'Select where to start from…' ),
 					},
 					{
 						key: CREATE_EMPTY_OPTION_VALUE,
-						name: __( 'Create Empty' ),
+						name: __( 'Create empty menu' ),
 					},
+					...( hasPages
+						? [
+								{
+									key: CREATE_FROM_PAGES_OPTION_VALUE,
+									name: __(
+										'Create from all top-level pages'
+									),
+								},
+						  ]
+						: [] ),
 			  ];
 
 	// If we don't have existing items or the User hasn't
@@ -297,7 +318,7 @@ function Navigation( {
 									} }
 									disabled={ ! selectedMenu }
 								>
-									{ __( 'Create from Menu' ) }
+									{ __( 'Create' ) }
 								</Button>
 							</>
 						) }
